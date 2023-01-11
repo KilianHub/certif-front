2fc37044a439ad74174adf09b933688876dff681
Alois Patoor <palois89@posteo.net>
2023-01-11 11:59:27 +0100

    button logout OK

--- src/app/ui/ui/ui.component.ts
+++ src/app/ui/ui/ui.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PopupService } from '@ng-bootstrap/ng-bootstrap/util/popup';
 
 @Component({
   selector: 'app-ui',
@@ -7,12 +8,21 @@ import { Component } from '@angular/core';
 })
 export class UiComponent {
   public close: boolean;
+  public showPopup: boolean;
 
   constructor() {
     this.close = false;
+    this.showPopup = false;
   }
 
   public toggle() {
     this.close = !this.close;
   }
+
+  public logout(){
+    console.log("clicked");
+    this.showPopup = !this.showPopup;
+  }
+
+
 }
